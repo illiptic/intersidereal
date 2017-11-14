69832c4699a7a9b3a3c2fe46668571d02136d36c
illiptic <jeremy.davis42@gmail.com>
2017-11-14 09:55:57 +0900

    Refactor planets

--- src/planet.js
+++ src/planet.js
@@ -3,54 +3,43 @@ import {loadTexture} from './loaders.js'
 
 const path = 'planets/'
 
-const textures = [
-  'earthmap.jpg',
-  'jupitermap.jpg',
-  'marsmap.jpg',
-  'mercurymap.jpg',
-  'plutomap.jpg',
-  'saturnmap.jpg',
-  'uranusmap.jpg',
-  'venusmap.jpg',
-]
-
-const bumpmaps = [
-  'earthbump.jpg',
-  'jupitermap.jpg',
-  'marsbump.jpg',
-  'mercurybump.jpg',
-  'plutobump.jpg',
-  'saturnmap.jpg',
-  'uranusmap.jpg',
-  'venusbump.jpg'
+const maps = [
+  { texture: 'earthmap.jpg', bump: 'earthbump.jpg'},
+  { texture: 'jupitermap.jpg', bump: 'jupitermap.jpg'},
+  { texture: 'marsmap.jpg', bump: 'marsbump.jpg'},
+  { texture: 'mercurymap.jpg', bump: 'mercurybump.jpg'},
+  { texture: 'plutomap.jpg', bump: 'plutobump.jpg'},
+  { texture: 'saturnmap.jpg', bump: 'saturnmap.jpg'},
+  { texture: 'uranusmap.jpg', bump: 'uranusmap.jpg'},
+  { texture: 'venusmap.jpg', bump: 'venusbump.jpg'}
 ]
 
 export default class Planet {
   constructor (generator) {
-    // let x = generator.random() * 1000000000 - 500000000
-    // let y = generator.random() * 1000000000 - 500000000
-    // let z = generator.random() * 1000000000 - 500000000
     this.orbitRadius = generator.random() * 742000000 + 58000000
     this.orbitAngle = generator.random() * 2 * Math.PI
+    this.orbitSpeed = 0.00000001
     this.orbitPlane = this.generateOrbit(generator)
-    let size = this.radius = generator.random() * 100000 + 2500
+    this.radius = generator.random() * 100000 + 2500
+    this.rotation = Math.PI / 720 / 60 // ~1 deg/s
 
+    this.createMesh(generator, this.radius)
+  }
+
+  createMesh (generator, size) {
     let i = Math.floor(generator.random() * 8)
-    let texture = textures[i]
-    let bumpmap = bumpmaps[i]
+    let {texture, bump} = maps[i]
 
     this.mesh = new THREE.Mesh(
       new THREE.SphereBufferGeometry(size, 32, 32),
       new THREE.MeshPhongMaterial({
         map: loadTexture(path+texture),
-        bumpMap: loadTexture(path+bumpmap),
+        bumpMap: loadTexture(path+bump),
         shininess: 30
       })
     );
-    this.mesh.name = 'planet'
 
-    this.mesh.position.copy(this.getPosition(this.orbitAngle, this.orbitPlane.a, this.orbitPlane.b, this.orbitRadius))
-    this.rotation = Math.PI / 720 / 60 // ~1 deg/s
+    this.mesh.name = 'planet'
   }
 
   /*
@@ -86,7 +75,7 @@ export default class Planet {
 
   update () {
     this.mesh.rotation.y += this.rotation
-    this.orbitAngle = (this.orbitAngle + 0.0001)%(2 * Math.PI)
+    this.orbitAngle = (this.orbitAngle + this.orbitSpeed)%(2 * Math.PI)
     this.mesh.position.copy(this.getPosition(this.orbitAngle, this.orbitPlane.a, this.orbitPlane.b, this.orbitRadius))
   }
 }
